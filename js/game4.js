0cbddce2534cf08c6acb771e71c6e398121d7d60
Aaron <tim2464@yahoo.com.tw>
2019-10-02 12:30:44 -0700

    modify game 4 function

--- js/game4.js
+++ js/game4.js
@@ -5,13 +5,15 @@ function lottery(numA) {
     array.push(numA);
     console.log("enter " + array);
     if (array.length == 3) {
-        if (array[0] == 1 && array[1] == 1 && array[2] == 4) {
+        if (array.includes(1) && array.includes(2) && array.includes(4)) {
             document.getElementById("result").innerHTML = "all the numbers are correct(even the order):  $10,000 prize";
             array = [];
-        } else if (numA == 1 && array[1] == 1 || array[1] == 1 && array[2] == 4 || array[2] == 4 && numA == 1) {
+        } else if (array.includes(1) && array.includes(4) ||
+            array.includes(2) && array.includes(4) ||
+            array.includes(1) && array.includes(2)) {
             document.getElementById("result").innerHTML = "all the numbers are correct, but not order: $3,000 prize";
             array = [];
-        } else if (numA == 1 || array[1] == 1 || array[2] == 4) {
+        } else if (array.includes(1) || array.includes(4) || array.includes(2)) {
             document.getElementById("result").innerHTML = "two numbers are correct out of three numbers:$1,000 prize";
             array = [];
         } else {
